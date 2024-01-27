35e12797b036934bf3c31993cf4c2915448919bf
Jesperi Laine <115222601+J-Laine@users.noreply.github.com>
2024-01-27 18:58:00 +0200

    Books dissappear hitting target

--- src/book.py
+++ src/book.py
@@ -1,6 +1,6 @@
 # book.py
 import pygame
-
+import time
 
 class Book:
     def __init__(self, x, y, target_pos):
@@ -10,6 +10,7 @@ class Book:
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.speed = 1
+        self.hit_time = None
 
     def draw(self, screen):
         screen.blit(self.image, self.pos)
@@ -24,3 +25,8 @@ class Book:
             self.pos[1] += self.speed
         elif self.pos[1] > self.target_pos[1]:
             self.pos[1] -= self.speed
+
+        # Check if the book has become stationary
+        if self.pos == self.target_pos:
+            # Set hit_time to the current time
+            self.hit_time = time.time()
